a6766ba5a2b93a6451d546c8307ad09e2b269e24
Janet Mendez <janetmendez.03@gmail.com>
2019-09-30 11:09:24 -0400

    populated data from backend

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import './App.css';
 import PlaylistContainer from './components/PlaylistContainer'
+import Navigation from './components/Navigation'
 
-function App() {
-
-  return (
-    <div className="App">
-    <PlaylistContainer/>
-    {/* <SongContainer/> */}
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    currentUser : {
+      "id": 1,
+      "name": "Taryn"
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+      <Navigation currentUser={this.state.currentUser}/> 
+      <PlaylistContainer currentUser={this.state.currentUser}/>
+      {/* <SongContainer/> */}
+      </div>
+    )
+  }
+  
 }
 
 export default App;
--- src/components/SongContainer.js
+++ src/components/SongContainer.js
@@ -2,9 +2,25 @@ import React from "react";
 import Song from "./Song";
 
 class SongContainer extends React.Component {
+    renderTracks = () => {
+        if(this.props.pickedPlaylist.tracks !== 0){
+            let tracks = this.props.pickedPlaylist.tracks
+            return tracks.map( t => <li>{t.name} - {t.artist }</li>)
+        }
+        else{
+            return <li>This playlist has no tracks</li>
+        }
+    }
+
     render(){
         return (
-            <div>Song</div>
+            <div>
+                <h2>{this.props.pickedPlaylist.title}</h2>
+                <p>Created By: {this.props.pickedPlaylist.user.name}</p>
+                <ul className="tracks-list">
+                    {this.renderTracks()}
+                </ul>
+            </div>
         )
     }
 
