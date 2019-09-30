ac32b7f8975d15729641bc845f9d73b2ed7cf45f
Janet Mendez <janetmendez.03@gmail.com>
2019-09-30 19:06:23 -0400

    finished votes method to backend

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -5,18 +5,48 @@ class Playlist extends React.Component {
 
     state = {
         votes: this.props.playlist.votes,
-        uservotes: {}
+        uservotes: null
     }
     
     addVote = (type) => {
-        console.log("adding a vote", type)
-        /// MAKE POST REQUEST
+        let config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Acceept': 'application/json'
+            },
+            body: JSON.stringify({
+                upvote: type,
+                playlist_id: this.props.playlist.id,
+                user_id: this.props.currentUser.id
+            })
+        }
+        fetch('http://localhost:3000/api/v1/votes', config)
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({
+                votes: [
+                    ...this.state.votes,
+                    data
+                ],
+                uservotes: data
+            })
+        })
     }
 
     removeVote = (type) => {
         if (type === this.state.uservotes.upvote){
-            console.log("removing vote", type)
-            /// MAKE A DELETE
+            let config = {
+                method: 'DELETE'
+            }
+            fetch(`http://localhost:3000/api/v1/votes/${this.state.uservotes.id}`, config)
+                .then(resp => resp.json())
+                .then(data => {
+                    this.setState({
+                        votes: this.state.votes.filter(v => !(v.id === this.state.uservotes.id)),
+                        uservotes: null
+                    })
+                })
         }
         else{
             let config = {
@@ -42,9 +72,6 @@ class Playlist extends React.Component {
                     uservotes: data
                 })
             })
-            
-            console.log("patching this vote", type)
-            /// MAKE A PATCH REQUEST
         }
     }
 
@@ -64,7 +91,7 @@ class Playlist extends React.Component {
     render() {
         let upvotes = this.state.votes.filter(v => v.upvote)
         let downvotes = this.state.votes.filter(v => !v.upvote)
-        
+
         return (
             <div className="playlist__title" onClick={() => this.props.viewPlaylist(this.props.playlist)}>
                 {this.renderInfo()}
