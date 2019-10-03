2bab6234886c7b12494495d9c45717f18c2e603f
Dennis Wang <dwang0816@gmail.com>
2019-10-03 09:09:49 -0400

    finished visualizer

--- src/components/SongSpecs.js
+++ src/components/SongSpecs.js
@@ -14,35 +14,44 @@ class SongSpecs extends React.Component {
             <div> 
             {/* <audio src={this.props.track.preview} controls></audio> */}
 
-
+                    {/* <header name = "Access-Control-Allow-Origin" value = "*" />  */}
                     <div className="card">
-                    {
+                    {/* {
                     this.state.audioStatus2 === 'PAUSED' ? (
                         <i onClick={this.play2} className="iconfont icon-play"></i>
                     ) : <i onClick={this.pause2} className="iconfont icon-pause"></i>
-                    }<br/>
+                    }<br/> */}
                     <audio id="audio-element2"
                     // preload="true"
                     src={this.props.track.preview}
                     controls
+                    crossOrigin="anonymous"
                     >
                     </audio>
+
+                    {/* <p>meterCount: 20<br/>gap: 1</p> */}
+                    </div>
+                    <div>
                     <AudioSpectrum
+                    
                     height={200}
                     width={300}
                     audioId={'audio-element2'}
-                    capColor={'red'}
+                    capColor={'purple'}
                     capHeight={2}
                     meterWidth={2}
                     meterCount={512}
                     meterColor={[
-                        {stop: 0, color: 'pink'},
-                        {stop: 1, color: 'red'}
+                        {stop: 0, color: 'red'},
+                        {stop: 1, color: 'orange'},
+                        // {stop: 2, color: 'yellow'},
+                        // {stop: 3, color: 'green'},
+                        // {stop: 4, color: 'blue'},
+                        // {stop: 5, color: 'purple'},
                     ]}
                     gap={1}
                     />
-                    <p>meterCount: 20<br/>gap: 1</p>
-                </div>
+                    </div>
             </div>
         </div>
     )
